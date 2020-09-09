f5a55f339f4e426ee59d986b3c89a5927c9f575b
ffelipegupe <1560@holbertonschool.com>
2020-09-09 18:39:43 +0000

    Add: adding task 7 files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,10 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
+    def close(self):
+        """ Method for deserializing the JSON file to objects """
+        self.reload()
+
     def delete(self, obj=None):
         """ Deletes an object from the dictionary """
         if obj is not None:
