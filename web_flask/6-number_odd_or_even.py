da958384e189ac23d4079ebd8c13a72dcbf949be
ffelipegupe <1560@holbertonschool.com>
2020-09-09 18:19:43 +0000

    Edit: url

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,7 +44,7 @@ def numer_template(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_par(n):
     """ Display a HTML page only if n is an integer """
     if n % 2 == 0:
